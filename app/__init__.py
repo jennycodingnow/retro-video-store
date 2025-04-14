81f5c8740cd645cedba33c07a689a88e34dc3c05
Kelsey Steven <kelsey.steven@adadevelopersacademy.org>
2025-04-14 14:21:12 -0700

    Update __init__.py for current pattern used through Unit 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,21 @@
 from flask import Flask
 from .db import db, migrate
 from .models import customer, rental, video
-from .routes.customer_routes import customers_bp
-from .routes.rental_routes import rentals_bp
-from .routes.video_routes import videos_bp
+# Import Blueprints here
 import os
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
+    if config:
+        app.config.update(config)
+
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints here
-    app.register_blueprint(customers_bp)
-    app.register_blueprint(rentals_bp)
-    app.register_blueprint(videos_bp)
 
     return app
